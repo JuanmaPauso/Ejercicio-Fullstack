7ea1c2f04e93382c1a68f1aba5dde98aeb6453bc
JuanmaPauso <juanmapauso@gmail.com>
2021-05-11 11:33:36 +0200

    Crear pagina Author y enlazarla

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,10 @@ router.get('/', function(req, res, next) {
   res.render('index');
 });
 
+// Author page.
+router.get('/author', (req, res, next) => {
+  res.render('author');
+});
+
+
 module.exports = router;
